599700d73858ec913c3d8e7befcae5b4abbb0b51
marclevetin <marc@derbyhat.org>
2017-08-26 17:54:05 -0400

    added httpclient gem so that I could get bacon ipsum text each time the page was loaded.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,17 @@
 class StaticPagesController < ApplicationController
   def index
-
+    @opening = get_bacon(5)
+    @second = get_bacon(4)
+    @caption_a = get_bacon(1)
+    @caption_b = get_bacon(3)
+    @caption_c = get_bacon(2)
   end
+
+  private
+  def get_bacon(num_sentences)
+    client = HTTPClient.new
+    response = client.get("https://baconipsum.com/api/?type=all-meat&sentences=#{num_sentences}&start-with-lorem=1")
+    response.body[2..-3] #this cleans up the response to remove brackets and slashes.
+  end
+
 end
